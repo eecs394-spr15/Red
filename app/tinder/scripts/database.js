3517d753ca0506908f7d8f57d0cc5e66922f596b
Chunxiao <diaochunxiao@gmail.com>
2015-04-05 16:30:03 -0500

    Parse setup test
    parse setup

---
+++ app/tinder/scripts/database.js
@@ -0,0 +1,7 @@
+
+Parse.initialize("u2bfRPj1KCw1vgvHBcYL6rmcNz3UfsJV83Dd1AKB", "nVbK9d630ykRnibWoFrK7S9ntLkXveBPEGeXQDL7");
+
+var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save({foo: "bar"}).then(function(object) {
+});

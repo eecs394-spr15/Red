7c0b9100b5638ee60aa4d19c4961a29f4aeb7296
Chunxiao <diaochunxiao@gmail.com>
2015-04-05 16:30:32 -0500

    Uncomment alert

--- app/tinder/scripts/database.js
+++ app/tinder/scripts/database.js
@@ -4,4 +4,5 @@ Parse.initialize("u2bfRPj1KCw1vgvHBcYL6rmcNz3UfsJV83Dd1AKB", "nVbK9d630ykRnibWoF
 var TestObject = Parse.Object.extend("TestObject");
 var testObject = new TestObject();
 testObject.save({foo: "bar"}).then(function(object) {
+  //alert("yay! it worked");
 });

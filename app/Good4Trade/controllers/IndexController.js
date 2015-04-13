b3f1529d05ea2bec994ec2da6a47b1d69134c8e3
Yang Hu <yanghu2019@u.northwestern.edu>
2015-04-12 23:20:47 -0500

    display uploaded image

--- app/Good4Trade/controllers/IndexController.js
+++ app/Good4Trade/controllers/IndexController.js
@@ -2,18 +2,25 @@ g4tapp.controller("IndexController", function($scope,supersonic){
   $scope.items = [
 	{url: 'http://images.craigslist.org/00303_8EJbdKNmkNQ_600x450.jpg',
 	title: 'whatever',
-	description: 'whatever'}
+	description: 'whatever',
+	picture: 'whatever'}
   ];
   Parse.initialize("eQLx1O6y08roi9FxLvTY5lOLdFeZ3NtmHO0tTNQF", "0fJ1VZtzTJS2d2FC4U4DxUscRYGF6Ix5Jg60W5rn");
   var ItemForSale = Parse.Object.extend("ItemForSale");
   var query = new Parse.Query(ItemForSale);
+   $scope.findItemsByUser = function(userID){
+	// TO DO
+        
+  }
+   
   query.descending("createdAt");
   query.limit(10);
   query.first().then(function(firstItem){
  	$scope.items = [
 	{url: firstItem.get("url"),
 	title: firstItem.get("title"),
-	description: firstItem.get("description")}
+	description: firstItem.get("description"),
+	picture: firstItem.get("picture").url()}
   ];
   });
 
@@ -23,12 +30,11 @@ g4tapp.controller("IndexController", function($scope,supersonic){
 	$scope.url =  iItem.get("url");
 	$scope.title = iItem.get("title");
 	$scope.description = iItem.get("description");	
-        $scope.items.push({url:$scope.url,title:$scope.title,description:$scope.description});
+        $scope.picture = iItem.get("picture").url();
+	$scope.items.push({url:$scope.url,title:$scope.title,description:$scope.description, picture:$scope.picture});
 	}
   });
-// $scope.url = 'http://images.craigslist.org/00303_8EJbdKNmkNQ_600x450.jpg';	
 
- // $scope.newItem = {title: 'tt', description: '', picture: ''};
 
 $scope.newItem = [
 	{url: 'http://images.craigslist.org/00303_8EJbdKNmkNQ_600x450.jpg',
@@ -48,21 +54,30 @@ $scope.uploadFile = function(files) {
 
 };
 
-	$scope.saveImage =function(){
+//var userQuery = new Parse.Query(Parse.User);
+
+
+$scope.saveImage =function(){
           var itemForSale = new ItemForSale();
 	  itemForSale.set("title", $scope.newItem.title);
           itemForSale.set("description", $scope.newItem.description);
 	  itemForSale.set("userID", $scope.newItem.userID); 
-	 /* var fileUpload = $("#uploadFile")[0];
-          if(fileUpload.files.length>0){
-		var file = fileUpload.files[0];
-		var name = "photo.jpg";
-		var parseFile = new Parse.File(name,file);
-	  }
-	  */
-	  var parseFile = new Parse.File("photo.jpg", {base64:$scope.imageData});
+          var parseFile = new Parse.File("photo.jpg", {base64:$scope.imageData});
 	  itemForSale.set("picture", parseFile);
 	  itemForSale.save();
+
+/*       
+	  // adding object ID to user class
+	  
+	  var objectID = itemForSale.id;
+	  alert(objectID);
+	  userQuery.equalTo("phone",$scope.newItem.userID);
+	  userQuery.find().then(function(thisUser){
+		thisUser.myItems.add(objectID);
+		alert("yes!!");
+	  });
+	  userQuery.save();
+*/
           document.getElementById('test').innerHTML=$scope.imageData;
 	};
 	$scope.cancel = function(){
@@ -72,19 +87,17 @@ $scope.uploadFile = function(files) {
 	var options = {
 	  quality: 50,
 	  allowEdit: true,
-	  targetWidth: 300,
-	  targetHeight: 300,
+	  targetWidth: 400,
+	  targetHeight: 500,
 	  //encodingType: "png",
-	  saveToPhotoAlbum: true,
+	  saveToPhotoAlbum: false,
 	  destinationType: "dataURL"
 	};
 
 
 	$scope.getPicture = function(){
 		
-		navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
-    	destinationType: Camera.DestinationType.DATA_URL
-		});
+		navigator.camera.getPicture(onSuccess, onFail, options);
 		
 		/*supersonic.media.camera.takePicture(options).then( function(result){
 	  		$scope.imageData= "data:image/jpeg;base64, " + result;
@@ -107,24 +120,4 @@ $scope.uploadFile = function(files) {
 	}
 
 });
-/*
-.directive('fileread', function () {
-    return {
-        scope: {
-            fileread: "="
-        },
-        link: function (scope, element, attributes) {
-            element.bind("change", function (changeEvent) {
-                var reader = new FileReader();
-                reader.onload = function (loadEvent) {
-                    scope.$apply(function () {
-                        scope.fileread = loadEvent.target.result;
-                    });
-                }
-                reader.readAsDataURL(changeEvent.target.files[0]);
-            });
-        }
-    }
-});
-*/
 

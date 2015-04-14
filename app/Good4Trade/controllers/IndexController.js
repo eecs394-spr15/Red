46074104ea43cb4b03114230eea1ac2aa4b1e216
Haomin Zeng <haominzeng2014@u.northwestern.edu>
2015-04-14 18:02:48 -0500

    modified items page- to provide offer
    Still need to work on Offer button, currently just show match. Should
    link to it to parse to add current item to “offeredItems”

--- app/Good4Trade/controllers/IndexController.js
+++ app/Good4Trade/controllers/IndexController.js
@@ -134,23 +134,15 @@ g4tapp.controller("IndexController", function($scope,supersonic){
 	        }
 	      });
 
-	
+	//controller from ItemsController.js
+
+	$scope.showMatch = function(){
+		supersonic.ui.dialog.alert("You Have a Match!!", options).then(function() {
+	  	supersonic.logger.log("Alert closed.");
+	});
+	}
 
 });
 
-//controller from ItemsController.js
-	// var options = {
-	//   message: "\n\n Contact Liam at:\n (781)-801-24822",
-	//   buttonLabel: "Close"
-	// };
 
-	// $scope.cancel = function(){
-	// 	supersonic.ui.modal.hide();
-	// }
-
-	// $scope.showMatch = function(){
-	// 	supersonic.ui.dialog.alert("You Have a Match!!", options).then(function() {
-	//   	supersonic.logger.log("Alert closed.");
-	// });
-	// }
 

ba3f115c0e5c293c894d9cd872811b91736795a6
aaronlohwy <aaronlohwy@gmail.com>
2015-04-22 17:50:58 -0500

    added remove button
    remove button implemented in your item description

--- app/Good4Trade/controllers/DescriptionController.js
+++ app/Good4Trade/controllers/DescriptionController.js
@@ -20,6 +20,7 @@ g4tapp.controller("DescriptionController", function($scope,supersonic){
 				$scope.iTitle = myItem.get("title");
 				$scope.iWishList = myItem.get("wishList");
 				$scope.iDescription = myItem.get("description");
+				$scope.myItem = myItem;
 				},
 			error: function(object, error) {
 			    alert("retrieving offered items failed!");
@@ -37,5 +38,73 @@ g4tapp.controller("DescriptionController", function($scope,supersonic){
 			}
 		});
 	}
+
+	var currentUser = Parse.User.current();
+
+	var queryToDeleteMyItem = new Parse.Query(ItemForSale);
+
+	$scope.removeThisItem = function(myItem){
+		var myArrayOfItems = currentUser.get("myItems");
+		var index = myArrayOfItems.indexOf(myItem.id);
+		if (index > -1) {
+			myArrayOfItems.splice(index, 1);
+		}
+		currentUser.set("myItems",myArrayOfItems);
+		currentUser.save();
+
+		//getting all objects that may have a link to this object
+		var offeredToArray = myItem.get("offeredTo");
+		var offeredFromArray = myItem.get("offeredItems");
+		var matchedItemsArray = myItem.get("matchedItemIDs");
+		var arrayToDeleteRef = offeredToArray.concat(offeredFromArray);
+		arrayToDeleteRef = arrayToDeleteRef.concat(matchedItemsArray);
+
+		//updating "offeredItems" and "matchedItemIDs" and "offeredTo" field of objects that there is a link to
+		queryToDeleteMyItem.containedIn("objectId", arrayToDeleteRef);
+	    queryToDeleteMyItem.find().then(function(results){
+	    	results.forEach(function(oItem){
+
+	    		oItem.remove("offeredItems", myItem.id);
+	    		oItem.remove("matchedItemIDs", myItem.id);
+	    		oItem.remove("offeredTo", myItem.id);
+	    		oItem.save();
+
+				asynchCallToRemoveRelations(myItem, oItem);
+			});
+	    });
+
+		//updating master list
+		myItem.destroy({
+			  success: function(myObject) {
+			    var options = {
+				  message: "Item has been removed from your account.",
+				  buttonLabel: "Close"
+				};
+
+				supersonic.ui.dialog.alert("Success!", options).then(function() {
+				  supersonic.logger.log("Alert closed.");
+				});
+			  },
+			  error: function(myObject, error) {
+			    var options = {
+				  message: "Item could not be removed from your account.",
+				  buttonLabel: "Close"
+				};
+
+				supersonic.ui.dialog.alert("Issue Encountered", options).then(function() {
+				  supersonic.logger.log("Alert closed.");
+				});
+			  }
+		});
+	}
+
+	function asynchCallToRemoveRelations(myItem, matchedItem){
+		var relation = myItem.relation("matchedItem");
+		relation.remove(matchedItem);
+		var theirRelation = matchedItem.relation("matchedItem");
+		theirRelation.remove(myItem);
+		myItem.save();
+		matchedItem.save();
+  	}
 
 });
